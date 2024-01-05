6e87c6649888a06df14234dc67de872428687d23
Bachwa Kienan <kienan42@gmail.com>
2024-01-05 20:44:09 +0100

    first

---
+++ routes/notionApi.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const {Client} = require('@notionhq/client');
+
+router.post('/database', async function (req, res, next) {
+
+    const {databaseId, notionApiKey} = req.body;
+
+    console.log(`Received database id ${databaseId} and notion api key ${notionApiKey}`)
+
+    const notion = new Client({auth: notionApiKey});
+
+    console.log("Querying Notion API")
+
+    const response = await notion.databases.query({
+        database_id: databaseId,
+    });
+    console.log("Response from Notion API", response);
+    res.send(response);
+});
+
+
+router.post('/patch', async function (req, res) {
+    const {notionApiKey, pageId, patchedProperties} = req.body;
+    console.log(`Received patch request for page id ${pageId} and notion api key ${notionApiKey}`)
+
+    console.log(req.body);
+    const notion = new Client({auth: notionApiKey});
+
+    //TODO to update database rows, use this instead https://developers.notion.com/reference/patch-page
+    
+    const response = await notion.pages.update({
+            auth: notionApiKey,
+            page_id: pageId,
+            properties: patchedProperties
+        }
+    );
+    console.log("Response from Notion API", response);
+    res.send(response);
+});
+
+module.exports = router;
---
+++ routes/steamApi.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+steamGames = [];
+steamGamesApiUrl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/?access_token="
+steamGameInfoApiUrl = "https://store.steampowered.com/api/appdetails"
+
+router.post('/steamGames', async function (req, res, next) {
+
+    const {steamApiKey} = req.body;
+
+    console.log(`Received request to retrieve steam games`)
+
+    try {
+        const response = await axios.get(steamGamesApiUrl + steamApiKey);
+        console.log(response);
+        if (response.status === 200) {
+            const data = response.data;
+            res.json(data);
+        } else {
+            res.status(response.status).send(response.statusText);
+        }
+    } catch (error) {
+        console.error('Error calling Steam API:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post("gameInfo", async function (req, res) {
+
+    const {appId, steamUserId} = req.body;
+
+    try {
+        const response = await axios.get(steamGameInfoApiUrl + "?appids=" + appId + "&steamid=" + steamUserId);
+        console.log(response);
+        if (response.status === 200) {
+            const data = response[appId].data;
+            res.json(data);
+        } else {
+            res.status(response.status).send(response.statusText);
+        }
+    } catch (error) {
+        console.error('Error calling Steam API:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+
+});
+
+module.exports = router;
